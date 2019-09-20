36fe0f585346dfadeaeb96df0cd8fc03e0384684
murasaqi <murasaqi@wahtever.co>
2019-09-20 21:36:32 +0900

    WIP

--- Assets/CameraSwitcher.cs
+++ Assets/CameraSwitcher.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraSwitcher : MonoBehaviour
 {
+    private Toggle Camera01;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+    public void Switch()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/ObjectDisable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDisable : MonoBehaviour
+{
+    public List<GameObject> Objes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void AllDisable()
+    {
+        foreach (var VARIABLE in Objes)
+        {
+            VARIABLE.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
